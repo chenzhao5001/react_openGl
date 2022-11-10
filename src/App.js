2e752521d06aa6598bf5e8e05e568c0a396a206b
chenzhao <chenzhao5001@163.com>
2022-11-10 21:22:17 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import OpenGlView from './view/OpenGlView'
+
+function App() {
+  return (
+    <OpenGlView />
+  );
+}
+
+export default App;
---
+++ src/view/OpenGlView.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './wrap.css'
+class OpenGlView extends React.Component {
+  render() {
+    return <div className={'wrap'}>222</div>
+  }
+}
+
+export default OpenGlView
