952330b98742651a8cd4aa3965fa340f565b9374
chenzhao <chenzhao5001@163.com>
2022-11-16 21:24:19 +0800

    opengl

--- src/view/OpenGlView.js
+++ src/view/OpenGlView.js
@@ -5,18 +5,26 @@ class OpenGlView extends React.Component {
   dom = null;
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,1,2000);
-  componentDidMount() {
-    console.log(THREE,"three")
+  renderer = new THREE.WebGLRenderer();
 
+
+  animate = () => {
+    // console.log(this.animate,"animate!!!!")
+    requestAnimationFrame( this.animate );
+    this.renderer.render( this.scene, this.camera );
+  }
+
+
+  componentDidMount() {
     this.camera.position.set(0,0,10);
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
     const cube = new THREE.Mesh( geometry, material );
     this.scene.add( cube );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.render(this.scene,this.camera)
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    this.dom.appendChild( renderer.domElement);
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.render( this.scene, this.camera );
+    this.dom.appendChild( this.renderer.domElement);
+    this.animate();
   }
 
   render() {
