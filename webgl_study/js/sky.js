57b592e34b6a9c7dad1514b30506d20337ff1669
chenzhao <chenzhao5001@163.com>
2022-11-21 09:45:17 +0800

    调整

---
+++ webgl_study/js/sky.js
@@ -0,0 +1,25 @@
+export default class Sky {
+  constructor(gl) {
+    this.gl = gl;
+    this.children = [];
+  }
+
+  add(obj) {
+    obj.gl = this.gl;
+    this.children.push(obj)
+  }
+
+  updateVertices(params) {
+    this.children.forEach(els => {
+      els.updateVertices(params);
+    })
+  }
+
+  draw() {
+    this.children.forEach(ele => {
+      ele.init();
+      ele.draw();
+    })
+  }
+
+}
